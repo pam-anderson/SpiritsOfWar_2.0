de1b026bc6a71fa5c2c892c0c81ad3fc8ad4870b
Pamela Group 2 <pamviolet.a@gmail.com>
2015-03-12 11:42:29 -0700

    Add turns for players

--- Pi/src/Player.py
+++ Pi/src/Player.py
@@ -1,12 +1,16 @@
+from enum import Enum
 
 # GLOBALS
 CHARS_PER_PLAYER = 3
+
+class Turn(Enum):
+    Move, Attack, Done = range(3)
 
 class CharacterClass:
     def __init__(self, name, healthPoints, attack, defense, attackRange,
             movement):
         self.className = name
-        self.healthPoints = healthPoints
+        self.maxHp = healthPoints
         self.attack = attack
         self.defense = defense
         self.attackRange = attackRange
@@ -15,11 +19,23 @@ class CharacterClass:
 class Character:
     def __init__(self, characterClass, team, characterId):
         self.characterClass = characterClass
+        self.currentHp = characterClass.maxHp
         self.team = team
         self.characterId = characterId
         self.position = [0, 0] # [x coord, y coord]
+        self.move = Turn.Move
 
 class Player:
     def __init__(self, characters):
         self.characters = characters
         self.charactersRemaining = CHARS_PER_PLAYER
+
+    def isTurnDone(self):
+        for character in self.characters:
+            if character.move != Turn.Done and character.currentHp > 0:
+                return False
+        return True
+
+    def resetTurn(self):
+        for character in characters:
+            character.move = Turn.Move
