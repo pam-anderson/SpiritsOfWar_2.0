f9539d4493fb2b3833a1c83560be5bcd1a4d297a
Arjan Dhaliwal <arjandhaliwal@hotmail.com>
2015-03-14 22:44:36 -0700

    fixed bugs

--- Pi/src/GameDrawer.py
+++ Pi/src/GameDrawer.py
@@ -3,13 +3,12 @@ START_PIXEL_Y = 0
 MAP_SIZE = 8
 
 #Draw functions
-def drawMap():
+def drawMap(gameMap, players):
     for y in range(0, MAP_SIZE):
         for x in range(0, MAP_SIZE):
             drawSprite(x << 4 + START_PIXEL_X, y << 4 + START_PIXEL_Y,
                 gameMap.tiles[x][y].sprite)
-
-    drawCharacters()
+    drawCharacters(players)
     return
 
 def drawSprite(x, y, memory):
@@ -26,15 +25,14 @@ def drawHealthbar(player_id, character_id):
     #draw on DE2 send max health and current health
     return
 
-def drawCharacters():
+def drawCharacters(players):
     for p in players:
         for c in players.characters:
             drawSprite(c.position[0] << 4 + START_PIXEL_X, c.position[1] << 4
                 + START_PIXEL_Y, c.standingSprite)
-        
     return
 
-def animateToTile(ram_location, dx, dy, oldx, oldy, newx, newy, sprite_type):
+def animateToTile(gameMap, ram_location, dx, dy, oldx, oldy, newx, newy, sprite_type):
     for i in range(0, 16):
         drawSprite(oldx << 4 + START_PIXEL_X, oldy << 4 + START_PIXEL_Y,
             gameMap.tiles[oldx][oldy].sprite)
@@ -51,28 +49,28 @@ def animateToTile(ram_location, dx, dy, oldx, oldy, newx, newy, sprite_type):
         gameMap.tiles[oldx][oldy].sprite)
     return
 
-def animate(ram_location, oldx, oldy, newx, newy):
+def animate(gameMap, ram_location, oldx, oldy, newx, newy):
     dist = 1
     #movement music
     path = [0] * (gameMap.tiles[newx][newy].distance + 1)
-    getPath(newx, newy, path)
+    getPath(gameMap, newx, newy, path)
     while dist <= map[newx][newy].distance:
         if path[dist] == 0:
-            animateToTile(ram_location, 1, 0, oldx, oldy, oldx + 1, oldy, 0)
+            animateToTile(gameMap, ram_location, 1, 0, oldx, oldy, oldx + 1, oldy, 0)
             oldx += 1
         elif path[dist] == 1:
-            animateToTile(ram_location, -1, 0, oldx, oldy, oldx - 1, oldy, 2)
+            animateToTile(gameMap, ram_location, -1, 0, oldx, oldy, oldx - 1, oldy, 2)
             oldx -= 1
         elif path[dist] == 2:
-            animateToTile(ram_location, 0, 1, oldx, oldy, oldx, oldy + 1, 4)
+            animateToTile(gameMap, ram_location, 0, 1, oldx, oldy, oldx, oldy + 1, 4)
             oldy += 1
         else:
-            animateToTile(ram_location, 0, -1, oldx, oldy, oldx, oldy - 1, 6)
+            animateToTile(gameMap, ram_location, 0, -1, oldx, oldy, oldx, oldy - 1, 6)
             oldy -= 1
         dist += 1
     return
 
-def getPath(newx, newy, path):
+def getPath(gameMap, newx, newy, path):
     distance = gameMap.tiles[newx][newy].distance
     while gameMap.tiles[newx][newy].distance != 0:
         if newx > 0 and gameMap.tiles[newx][newy].distance > \
@@ -94,13 +92,12 @@ def getPath(newx, newy, path):
         distance -= 1
     return
 
-def movePlayer(player_id, character_id, oldx, oldy, newx, newy):
+def movePlayer(gameMap, character, oldx, oldy, newx, newy):
     gameMap.tiles[oldx][oldy].occupiedBy = 0
-        if newx == -1 or newy == -1 or newx == MAP_SIZE or newy == MAP_SIZE:
-            return
-        animate(players[player_id].characters[character_id].animationSprite, oldx, oldy, newx, newy)
-        players[player_id].characters[character_id].position[0] = newx
-        players[player_id].characters[character_id].position[1] = newy
-        gameMap.tiles[newx][newy].occupiedBy = players[player_id].characters[character_id]
-        drawSprite(newx << 4 + START_PIXEL_X, newy << 4 + START_PIXEL_Y,
-            players[player_id].characters[character_id].standingSprite)
+    if newx == -1 or newy == -1 or newx == MAP_SIZE or newy == MAP_SIZE:
+        return
+    animate(gameMap, character.animationSprite, oldx, oldy, newx, newy)
+    character.position[0] = newx
+    character.position[1] = newy
+    gameMap.tiles[newx][newy].occupiedBy = character
+    drawSprite(newx << 4 + START_PIXEL_X, newy << 4 + START_PIXEL_Y, character.standingSprite)
