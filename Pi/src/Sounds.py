c1529836c26e69fc2a3be68fc0713d8944bc90b1
Kryostat <e.kevin.xu@gmail.com>
2015-03-18 02:20:35 -0700

    Fixed spacing

--- Pi/src/Sounds.py
+++ Pi/src/Sounds.py
@@ -2,14 +2,14 @@ import pygame
 
 def init_music():
     pygame.mixer.init()  # initialize pygame
-	pygame.mixer.music.load("Gumi.wav") # load main music
-	pygame.mixer.music.play(-1) # -1 for infinite loop
+    pygame.mixer.music.load("Gumi.wav") # load main music
+    pygame.mixer.music.play(-1) # -1 for infinite loop
 
 def init_sfx():
-	global sfx # initialize global
-	sfx = [] # create an array of objects
-	sfx.append(pygame.mixer.Sound("Gumi.wav")) # can append more depending on # of SFX
+    global sfx # initialize global
+    sfx = [] # create an array of objects
+    sfx.append(pygame.mixer.Sound("Gumi.wav")) # can append more depending on # of SFX
 	
 def play_sfx(sfx_num):
-	sfx[sfx_num].play(0) #0 for repeat once 
+    sfx[sfx_num].play(0) #0 for repeat once 
 	
