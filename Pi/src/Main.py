e2b04e80a383853b595df0c20f3ef1717d0b9dcd
Kryostat <e.kevin.xu@gmail.com>
2015-03-18 19:58:51 -0700

    added sounds, gamecontroller.py still needs some lines for sfx

--- Pi/src/Main.py
+++ Pi/src/Main.py
@@ -1,7 +1,11 @@
 from GameController import Game
+from Sounds import Sound
 
 def main():
     game = Game()
+	sound = Sound()
+#   sound.init_music() # comment in for music. 
+#	sound.init_sfx()   # use command "sudo amixer cset numid=3 1"
     game.playGame()
 
 if __name__ == "__main__":
--- Pi/src/Sounds.py
+++ Pi/src/Sounds.py
@@ -1,17 +1,21 @@
 import pygame
-
+#run the following command first!
 #sudo amixer cset numid=3 1
+class Sound:
+    def init_music():
+        pygame.mixer.init()  # initialize pygame
+        pygame.mixer.music.load("mus.wav") # load main music
+        pygame.mixer.music.play(-1) # -1 for infinite loop
 
-def init_music():
-    pygame.mixer.init()  # initialize pygame
-    pygame.mixer.music.load("Gumi.wav") # load main music
-    pygame.mixer.music.play(-1) # -1 for infinite loop
-
-def init_sfx():
-    global sfx # initialize global
-    sfx = [] # create an array of objects
-    sfx.append(pygame.mixer.Sound("Gumi.wav")) # can append more depending on # of SFX
+    def init_sfx():
+        global sfx # initialize global
+        sfx = [] # create an array of objects
+        sfx.append(pygame.mixer.Sound("war.wav")) # can append more depending on # of SFX
+		sfx.append(pygame.mixer.Sound("arch.wav"))
+		sfx.append(pygame.mixer.Sound("mag.wav"))
+		sfx.append(pygame.mixer.Sound("mov.wav"))
+		sfx.append(pygame.mixer.Sound("die.wav"))
 	
-def play_sfx(sfx_num):
-    sfx[sfx_num].play(0) #0 for repeat once 
+    def play_sfx(sfx_num):
+        sfx[sfx_num].play(0) #0 for repeat once 
 	
