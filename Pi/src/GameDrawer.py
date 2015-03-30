a0c7a3b8ea0c2d502dcbdce63b07c0fdd5e88809
Pamela Group 2 <pamviolet.a@gmail.com>
2015-03-30 14:40:18 -0700

    Remove 2-way communication

--- Pi/src/GameDrawer.py
+++ Pi/src/GameDrawer.py
@@ -15,8 +15,8 @@ class Drawer:
         self.readyPin = 3
         self.donePin = 5
         self.messagePins = [7, 8, 10, 11]
-        self.dataPins = [12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 35,
-            36, 37, 38, 40]
+        self.dataPins = [12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32,
+            33, 35, 36, 37, 38, 40]
         self.setGpios()
 
     def __del__(self):
